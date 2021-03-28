24d051de4567ef56b904209e238177ffe771479c
keza1brenda <keza1brenda@gmail.com>
2021-03-29 01:15:14 +0200

    add departmetsdao

---
+++ src/main/java/dao/DepartmentsDao.java
@@ -0,0 +1,23 @@
+package dao;
+
+import models.DepartmentNews;
+import models.Departments;
+import models.Users;
+
+import java.util.List;
+
+public interface DepartmentsDao {
+    //create
+    void add(Departments departments);
+    void addUsersToDepartments(Users users, Departments departments);
+
+    //read
+    List<Departments> getAll();
+    List<Users> getAllUsersForADepartments(int id);
+    Departments findById(int id);
+    List<DepartmentNews> getDepartmentNews(int id);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+}
